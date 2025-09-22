8b8cc7bc2c6c34f8f33bdc98d436269de4784116
eowva <deshpand@umd.edu>
2025-09-22 19:02:19 -0400

    Commit 13?

--- layer.js
+++ layer.js
@@ -1,51 +1,55 @@
-import Graphic from "@arcgis/core/Graphic";
-import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/Graphic",
+  "esri/layers/GraphicsLayer",
+  "esri/geometry/Polygon"
+], function(Map, MapView, Graphic, GraphicsLayer, Polygon) {
 
-// Create a new GraphicsLayer
-const graphicsLayer = new GraphicsLayer();
+  const map = new Map({
+    basemap: "streets"
+  });
 
-// Example: Add a simple point graphic
-// const point = {
-//     type: "point",
-//     longitude: -118.805,
-//     latitude: 34.027
-// };
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    center: [-76.949682, 38.986166],
+    zoom: 17
+  });
 
-// Make a square polygon around a center
-function createSquare(center, halfSize) {
-  // halfSize = half the side length (in degrees if using wkid 4326)
-  return [
-    [center.x - halfSize, center.y - halfSize], // bottom-left
-    [center.x + halfSize, center.y - halfSize], // bottom-right
-    [center.x + halfSize, center.y + halfSize], // top-right
-    [center.x - halfSize, center.y + halfSize], // top-left
-    [center.x - halfSize, center.y - halfSize]  // close back
-  ];
-}
+  const graphicsLayer = new GraphicsLayer();
+  map.add(graphicsLayer);
 
-const center = { x: -76.949682, y: 38.986166 };
-const halfSize = 0.005; // ~0.5 km in degrees
-
-const squarePolygon = new Polygon({
-  rings: [createSquare(center, halfSize)],
-  spatialReference: { wkid: 4326 }
-});
-
-const squareGraphic = new Graphic({
-  geometry: squarePolygon,
-  symbol: {
-    type: "simple-fill",
-    color: [255, 165, 0, 0.4], // orange, semi-transparent
-    outline: {
-      color: "white",
-      width: 2
-    }
+  function createSquare(center, halfSize) {
+    return [
+      [center.x - halfSize, center.y - halfSize], 
+      [center.x + halfSize, center.y - halfSize],
+      [center.x + halfSize, center.y + halfSize], 
+      [center.x - halfSize, center.y + halfSize], 
+      [center.x - halfSize, center.y - halfSize]  
+    ];
   }
+
+  const center = { x: -76.949682, y: 38.986166 };
+  const halfSize = 0.0005; 
+
+  const squarePolygon = new Polygon({
+    rings: [createSquare(center, halfSize)],
+    spatialReference: { wkid: 4326 }
+  });
+
+  const squareGraphic = new Graphic({
+    geometry: squarePolygon,
+    symbol: {
+      type: "simple-fill",
+      color: [255, 165, 0, 0.4],
+      outline: {
+        color: "white",
+        width: 2
+      }
+    }
+  });
+
+  graphicsLayer.add(squareGraphic);
+
 });
-
-// Add it to your graphics layer
-graphicsLayer.add(squareGraphic);
-
-graphicsLayer.add(graphic);
-
-export default graphicsLayer;
